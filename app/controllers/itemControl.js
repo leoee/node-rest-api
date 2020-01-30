18057983c27facadede70f1f131443a6bc68114d
Leonardo Eufrazio <leonardoeufrazio@gec.inatel.br>
2020-01-30 21:11:33 -0200

    Finish draw for createItem

--- app/controllers/itemControl.js
+++ app/controllers/itemControl.js
@@ -1,6 +1,27 @@
+const Item = require('../models/itemModel.js');
+
 const createItem =  (req, res) => {
+    let item = new Item();
+    item.setId(1);
+    item.setName(req.body.name);
+    item.setAmount(req.body.amount);
+    item.setPrice(req.body.price);
+
+    console.log(item);
+
+    res.status(201).json({message: 'Item ' + item.getId() + ' was created!'});
+}
+
+const deleteItem =  (req, res) => {
+    const id = req.params.id;
+    res.status(200).json({message: 'Item ' + id + ' was deleted!'});
+}
+
+const getItem =  (req, res) => {
+    const id = req.params.id;
+    console.log(id);
     const item = req.body;
-    res.status(201).json({message: 'Item XXXX was created!'});
+    res.status(200).json({message: 'Item XXXX was created!'});
 }
 
 const updateItem = (req, res) => {
@@ -14,5 +35,7 @@ const updateItem = (req, res) => {
 
 module.exports = {
     createItem,
+    deleteItem,
+    getItem,
     updateItem
 }
--- app/routes/itemRoute.js
+++ app/routes/itemRoute.js
@@ -2,7 +2,8 @@ const itemControl = require('../controllers/itemControl');
 
 module.exports = app => {
     app.post('/item', itemControl.createItem);
-    // app.get('/item', );
+    app.get('/item', itemControl.getItem);
+    app.get('/item/:id', itemControl.getItem);
     app.put('/item/:id', itemControl.updateItem);
-    // app.delete('/item/:id', );
+    app.delete('/item/:id', itemControl.deleteItem);
 }
